d45b148abe05e6214a43ea0758b9bf9d296fd443
hellohyh001 <huiyuehong@huawei.com>
2022-02-26 15:56:27 +0800

    Signed-off-by:hellohyh001<huiyuehong@huawei.com>
    Signed-off-by: hellohyh001 <huiyuehong@huawei.com>

--- interfaces/plugin/src/sensor_napi_utils.cpp
+++ interfaces/plugin/src/sensor_napi_utils.cpp
@@ -274,7 +274,7 @@ void ConvertToSensorData(napi_env env, AsyncCallbackInfo *asyncCallbackInfo, nap
     int32_t sensorTypeId = asyncCallbackInfo->data.sensorData.sensorTypeId;
     if ((g_sensorAttributeList.count(sensorTypeId)) == 0
         || g_sensorAttributeList[sensorTypeId].size()
-        > (asyncCallbackInfo->data.sensorData.dataLength / sizeof(float)))) {
+        > (asyncCallbackInfo->data.sensorData.dataLength / sizeof(float))) {
         HiLog::Error(LABEL, "%{public}s count of sensorTypeId is zero", __func__);
         return;
     }
