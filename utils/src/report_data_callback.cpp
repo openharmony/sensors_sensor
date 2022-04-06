6201249f5742162cdb6e81521257b071026a2b82
li-yaoyao777 <liyaoyao7@huawei.com>
2022-04-06 15:14:32 +0800

    sensor_data_processer.cpp
    Signed-off-by: li-yaoyao777 <liyaoyao7@huawei.com>

--- utils/src/report_data_callback.cpp
+++ utils/src/report_data_callback.cpp
@@ -66,9 +66,11 @@ int32_t ReportDataCallback::ReportEventCallback(const struct SensorEvent* event,
     }
     int32_t leftSize = CIRCULAR_BUF_LEN - cb->eventsBuf_.eventNum;
     int32_t toEndLen = CIRCULAR_BUF_LEN - cb->eventsBuf_.writePosition;
-    if (leftSize < 0 || toEndLen < 0) {
-        SEN_HILOGE("callback or circularBuf or event cannot be null");
-        delete[] event->data;
+    if (leftSize < 0 || toEndLen < 0){
+        SEN_HILOGE("Leftsize and toendlen cannot be less than zero");
+        if (event->data != nullptr){
+            delete[] event->data;
+        }
         return ERROR;
     }
     if (toEndLen == 0) {
