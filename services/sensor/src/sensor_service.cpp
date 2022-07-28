2f54b291dd6902066f19f88d14acc321e120e8f3
wuzhihuitmac <wuzhihui4@huawei.com>
2022-07-28 01:36:47 +0000

    Add memset return value judgment
    Signed-off-by: wuzhihuitmac <wuzhihui4@huawei.com>
    Change-Id: Id1768d89d1cdd8d0c6868051bf74b6e1ae829b30

--- services/sensor/src/sensor_service.cpp
+++ services/sensor/src/sensor_service.cpp
@@ -38,6 +38,7 @@ constexpr HiLogLabel LABEL = { LOG_CORE, SENSOR_LOG_DOMAIN, "SensorService" };
 constexpr uint32_t INVALID_SENSOR_ID = -1;
 constexpr int32_t INVALID_PID = -1;
 constexpr int64_t MAX_EVENT_COUNT = 1000;
+constexpr int32_t MAX_DUMP_PARAMETERS = 32;
 enum {
     FLUSH = 0,
     SET_MODE,
@@ -425,8 +426,8 @@ void SensorService::UnregisterClientDeathRecipient(sptr<IRemoteObject> sensorCli
 int32_t SensorService::Dump(int32_t fd, const std::vector<std::u16string> &args)
 {
     CALL_LOG_ENTER;
-    if (fd < 0) {
-        SEN_HILOGE("fd is invalid");
+    if (fd < 0 || args.size() > MAX_DUMP_PARAMETERS) {
+        SEN_HILOGE("fd is invalid or wrong number of parameters");
         return DUMP_PARAM_ERR;
     }
     SensorDump &sensorDump = SensorDump::GetInstance();
