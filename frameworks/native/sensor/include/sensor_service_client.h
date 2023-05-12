685c0f8db9f2fe3f12af037d67cfab28f5b5a501
hui1975 <zhaominghui10@huawei.com>
2023-05-12 10:07:01 +0000

    省功耗客户端代码
    Signed-off-by: hui1975 <zhaominghui10@huawei.com>
    Change-Id: Id627b58083f0f11b8ed22ff022445538cb690190

--- frameworks/native/sensor/include/sensor_service_client.h
+++ frameworks/native/sensor/include/sensor_service_client.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,11 +17,13 @@
 #define SENSOR_SERVICE_CLIENT_H
 
 #include <map>
+#include <set>
 #include <vector>
 
 #include "iservice_registry.h"
 #include "singleton.h"
 
+#include "active_info.h"
 #include "sensor_agent_type.h"
 #include "sensor_basic_data_channel.h"
 #include "sensor_basic_info.h"
@@ -29,12 +31,13 @@
 #include "sensor_data_channel.h"
 #include "sensor.h"
 #include "sensor_service_proxy.h"
-
+#include "stream_socket.h"
 
 namespace OHOS {
 namespace Sensors {
-class SensorServiceClient : public Singleton<SensorServiceClient> {
+class SensorServiceClient : public StreamSocket, public Singleton<SensorServiceClient> {
 public:
+    ~SensorServiceClient() override;
     std::vector<Sensor> GetSensorList();
     int32_t EnableSensor(int32_t sensorId, int64_t samplingPeriod, int64_t maxReportDelay);
     int32_t DisableSensor(int32_t sensorId);
@@ -42,11 +45,20 @@ public:
     int32_t DestroyDataChannel();
     void ProcessDeathObserver(const wptr<IRemoteObject> &object);
     bool IsValid(int32_t sensorId);
+    int32_t SuspendSensors(int32_t pid);
+    int32_t ResumeSensors(int32_t pid);
+    int32_t GetActiveInfoList(int32_t pid, std::vector<ActiveInfo> &activeInfoList);
+    int32_t Register(SensorActiveInfoCB callback, sptr<SensorDataChannel> sensorDataChannel);
+    int32_t Unregister(SensorActiveInfoCB callback);
+    void ReceiveMessage(const char *buf, size_t size);
+    void Disconnect();
 
 private:
     int32_t InitServiceClient();
     void UpdateSensorInfoMap(int32_t sensorId, int64_t samplingPeriod, int64_t maxReportDelay);
     void DeleteSensorInfoItem(int32_t sensorId);
+    int32_t CreateSocketChannel();
+    void HandleNetPacke(NetPacket &pkt);
     std::mutex clientMutex_;
     sptr<IRemoteObject::DeathRecipient> serviceDeathObserver_;
     sptr<ISensorService> sensorServer_;
@@ -55,8 +67,11 @@ private:
     sptr<SensorClientStub> sensorClientStub_;
     std::mutex mapMutex_;
     std::map<int32_t, SensorBasicInfo> sensorInfoMap_;
+    std::atomic_bool isConnected_ = false;
+    CircleStreamBuffer circBuf_;
+    std::mutex activeInfoCBMutex_;
+    std::set<SensorActiveInfoCB> activeInfoCBSet_;
 };
 }  // namespace Sensors
 }  // namespace OHOS
-
 #endif  // SENSOR_SERVICE_CLIENT_H
