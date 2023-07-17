9833df75485f715551c0ea90d7aaf174d3c337e9
hui1975 <zhaominghui10@huawei.com>
2023-07-17 07:12:07 +0000

    部件隔离
    Signed-off-by: hui1975 <zhaominghui10@huawei.com>
    Change-Id: I488a8f7652fe48744b37d44d109e711adb324531

--- services/sensor/hdi_connection/adapter/src/hdi_connection.cpp
+++ services/sensor/hdi_connection/adapter/src/hdi_connection.cpp
@@ -19,7 +19,9 @@
 #include <thread>
 
 #include "hisysevent.h"
+#ifdef HAS_HDF_DRIVERS_INTERFACE_SENSOR
 #include "iproxy_broker.h"
+#endif
 #include "sensor_event_callback.h"
 #include "sensors_errors.h"
 #include "v1_1/isensor_interface.h"
