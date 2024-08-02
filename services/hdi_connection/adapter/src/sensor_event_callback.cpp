e702b704b1dbcb3bb31516a4fc7c7155b38e513f
wuzhihuitmac <wuzhihui4@huawei.com>
2024-08-02 11:18:44 +0800

    Added trigger sensor type filtering
    Signed-off-by: wuzhihuitmac <wuzhihui4@huawei.com>
    Change-Id: I536623ea60b6b3818a9ead659921fd9426257a93

--- services/hdi_connection/adapter/src/sensor_event_callback.cpp
+++ services/hdi_connection/adapter/src/sensor_event_callback.cpp
@@ -14,6 +14,8 @@
  */
 #include "sensor_event_callback.h"
 
+#include <set>
+
 #include "hdi_connection.h"
 #include "sensor_agent_type.h"
 #include "sensor_errors.h"
@@ -34,6 +36,13 @@ enum {
     THREE_DIMENSION = 3,
     SEVEN_DIMENSION = 7,
     DEFAULT_DIMENSION = 16
+};
+const std::set<int32_t> g_sensorTypeTrigger = {
+    SENSOR_TYPE_ID_PROXIMITY,
+    SENSOR_TYPE_ID_DROP_DETECTION,
+    SENSOR_TYPE_ID_HALL,
+    SENSOR_TYPE_ID_HALL_EXT,
+    SENSOR_TYPE_ID_PROXIMITY1
 };
 } // namespace
 
@@ -56,8 +65,7 @@ int32_t SensorEventCallback::OnDataEvent(const HdfSensorEvents &event)
         .mode = event.mode,
         .dataLen = event.dataLen
     };
-    if (sensorData.sensorTypeId == SENSOR_TYPE_ID_PROXIMITY ||
-        sensorData.sensorTypeId == SENSOR_TYPE_ID_DROP_DETECTION) {
+    if (g_sensorTypeTrigger.find(sensorData.sensorTypeId) != g_sensorTypeTrigger.end()) {
         sensorData.mode = SENSOR_ON_CHANGE;
     }
     CHKPR(sensorData.data, ERR_NO_INIT);
