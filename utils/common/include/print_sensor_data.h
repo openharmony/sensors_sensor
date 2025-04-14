69ed837b81932e2f351ef064538cff7bf3417c4a
wuzhihuitmac <wuzhihui4@huawei.com>
2025-04-14 17:35:40 +0800

    Add times for log print
    Signed-off-by: wuzhihuitmac <wuzhihui4@huawei.com>
    Change-Id: I2a293cb785192e57d60b51ca38809c6eb0cfe154

--- utils/common/include/print_sensor_data.h
+++ utils/common/include/print_sensor_data.h
@@ -39,6 +39,7 @@ public:
     void RemovePrintUserInfo(const RecordSensorCallback callback);
     void PrintSensorDataLog(const std::string &name, const SensorData &data);
     void PrintSensorInfo(SensorInfo *sensorInfos, int32_t sensorInfoCount);
+    void ResetClientTimes();
 
 private:
     void PrintClientData(const SensorEvent &event);
@@ -49,6 +50,7 @@ private:
     struct LogPrintInfo {
         int32_t count { 0 };
         int64_t lastTime { 0 };
+        uint64_t hdiTimes { 0 };
     };
     std::mutex hdiLoginfoMutex_;
     std::mutex clientLoginfoMutex_;
@@ -62,6 +64,7 @@ private:
         {SENSOR_TYPE_ID_MAGNETIC_FIELD, info_},
     };
     std::map<RecordSensorCallback, LogPrintInfo> clientLoginfo_;
+    uint64_t clientTimes_ = 0;
 };
 } // namespace Sensors
 } // namespace OHOS
