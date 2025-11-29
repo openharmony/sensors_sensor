0fd314775a38dab7ca2181832933566fd2a2dbd4
wuzhihuitmac <wuzhihui4@huawei.com>
2025-11-29 14:41:06 +0800

    add isMockSensor function
    Signed-off-by: wuzhihuitmac <wuzhihui4@huawei.com>

--- interfaces/inner_api/sensor_agent_type.h
+++ interfaces/inner_api/sensor_agent_type.h
@@ -127,7 +127,7 @@ typedef struct SensorInfo {
     int32_t deviceId = -1;        /**< Device ID */
     int32_t location = -1;        /**< Is the device a local device or an external device */
     int32_t sensorIndex = -1;     /**< Sensor Index */
-    bool isMockSensor = true; /**< Is virtual sensor or real sensor */
+    bool isMockSensor = true; /**< Is the device a mock device or a real device */
 } SensorInfo;
 
 /**
